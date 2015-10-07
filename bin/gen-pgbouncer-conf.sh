a4e692fdc183a8c9a449705ecc8155bc75d1b84a
Michael Hahn <mwhahn@gmail.com>
2015-10-07 00:00:01 -0700

    Don't use stunnel

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -3,34 +3,12 @@
 POSTGRES_URLS=${PGBOUNCER_URLS:-DATABASE_URL}
 POOL_MODE=${PGBOUNCER_POOL_MODE:-transaction}
 SERVER_RESET_QUERY=${PGBOUNCER_SERVER_RESET_QUERY}
-n=1
 
 # if the SERVER_RESET_QUERY and pool mode is session, pgbouncer recommends DISCARD ALL be the default
 # http://pgbouncer.projects.pgfoundry.org/doc/faq.html#_what_should_my_server_reset_query_be
 if [ -z "${SERVER_RESET_QUERY}" ] &&  [ "$POOL_MODE" == "session" ]; then
   SERVER_RESET_QUERY="DISCARD ALL;"
 fi
-
-# Enable this option to prevent stunnel failure with Amazon RDS when a dyno resumes after sleeping
-if [ -z "${ENABLE_STUNNEL_AMAZON_RDS_FIX}" ]; then
-  AMAZON_RDS_STUNNEL_OPTION=""
-else
-  AMAZON_RDS_STUNNEL_OPTION="options = NO_TICKET"
-fi
-
-mkdir -p /app/vendor/stunnel/var/run/stunnel/
-cat >> /app/vendor/stunnel/stunnel-pgbouncer.conf << EOFEOF
-foreground = yes
-
-options = NO_SSLv2
-options = SINGLE_ECDH_USE
-options = SINGLE_DH_USE
-socket = r:TCP_NODELAY=1
-options = NO_SSLv3
-${AMAZON_RDS_STUNNEL_OPTION}
-ciphers = HIGH:!ADH:!AECDH:!LOW:!EXP:!MD5:!3DES:!SRP:!PSK:@STRENGTH
-debug = ${PGBOUNCER_STUNNEL_LOGLEVEL:-notice}
-EOFEOF
 
 cat >> /app/vendor/pgbouncer/pgbouncer.ini << EOFEOF
 [pgbouncer]
@@ -71,36 +49,23 @@ do
   DB_NAME=${DB_URI[4]}
   DB_MD5_PASS="md5"`echo -n ${DB_PASS}${DB_USER} | md5sum | awk '{print $1}'`
 
-  CLIENT_DB_NAME="db${n}"
-
   echo "Setting ${POSTGRES_URL}_PGBOUNCER config var"
 
   if [ "$PGBOUNCER_PREPARED_STATEMENTS" == "false" ]
   then
-    export ${POSTGRES_URL}_PGBOUNCER=postgres://$DB_USER:$DB_PASS@127.0.0.1:6000/$CLIENT_DB_NAME?prepared_statements=false
+    export ${POSTGRES_URL}_PGBOUNCER=postgres://$DB_USER:$DB_PASS@127.0.0.1:6000/$DB_NAME?prepared_statements=false
   else
-    export ${POSTGRES_URL}_PGBOUNCER=postgres://$DB_USER:$DB_PASS@127.0.0.1:6000/$CLIENT_DB_NAME
+    export ${POSTGRES_URL}_PGBOUNCER=postgres://$DB_USER:$DB_PASS@127.0.0.1:6000/$DB_NAME
   fi
 
-  cat >> /app/vendor/stunnel/stunnel-pgbouncer.conf << EOFEOF
-[$POSTGRES_URL]
-client = yes
-protocol = pgsql
-accept  = /tmp/.s.PGSQL.610${n}
-connect = $DB_HOST:$DB_PORT
-retry = ${PGBOUNCER_CONNECTION_RETRY:-"no"}
+  cat >> /app/vendor/pgbouncer/pgbouncer.ini << EOFEOF
+$DBNAME= host=${DB_HOST} port=${DB_PORT} user=${DB_USER} dbname=${DB_NAME}
 EOFEOF
 
   cat >> /app/vendor/pgbouncer/users.txt << EOFEOF
 "$DB_USER" "$DB_MD5_PASS"
 EOFEOF
 
-  cat >> /app/vendor/pgbouncer/pgbouncer.ini << EOFEOF
-$CLIENT_DB_NAME= dbname=$DB_NAME port=610${n}
-EOFEOF
-
-  let "n += 1"
 done
 
 chmod go-rwx /app/vendor/pgbouncer/*
-chmod go-rwx /app/vendor/stunnel/*
